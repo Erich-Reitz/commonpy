0e56221563d96818834b1c9134f35ba7b529b2f8
Erich-Reitz <alohae.reitz>
2022-06-20 18:49:59 -0400

    update jsonEncoder

--- json_utils.py
+++ json_utils.py
@@ -1,8 +1,12 @@
 import json
 import dataclasses
+import datetime
 
 class EnhancedJSONEncoder(json.JSONEncoder):
-        def default(self, o):
-            if dataclasses.is_dataclass(o):
-                return dataclasses.asdict(o)
-            return super().default(o)
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        if isinstance(o, (datetime.datetime, datetime.date, datetime.time)):
+            return o.isoformat()
+        
+        return super().default(o)
